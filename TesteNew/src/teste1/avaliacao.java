d74e96512a5c62791a9c0284cbabc7f6a189e450
nig0065765 <nig0065765@aluno.firjan.senaisesi.com.br>
2025-06-18 19:46:07 -0300

    acrescimo de comentarios e correção na abertura de melhor e pior nota

--- TesteNew/src/teste1/avaliacao.java
+++ TesteNew/src/teste1/avaliacao.java
@@ -9,29 +9,38 @@ public class avaliacao {
 
 Scanner teste = new Scanner(System.in);
 		
-		double mediaMelhor = 10;
+		//Variável de abertura par controlar o aluno com melhor e pior nota.
+		double mediaMelhor = 0;
 		String nomeMelhor = "";
 		
-		double mediaPior = 0;
+		double mediaPior = 10;
 		String nomePior = "";
 		
+		//Inicio para indicar a quantidade de repetições, em seguida o System para
+		//a leitura do nome.
 		for (int a = 1; a <= 10; a++) {
 			System.out.println("Digite o nome do Aluno: ");
 			String nome = teste.nextLine();
 			
+			//indicação para iniciar a soma e guardar 4 notas
+			//pede a indicação de 4 notas
 			double soma = 0;
 			for(int n = 1; n <= 4; n++) {
 				System.out.println("Digite a nota" + n + ":" );
 				double nota = teste.nextDouble();
 				
+				//somas as 4 notas que foram guardadas
 				soma += nota;
 			}
 		
 			teste.nextLine();
-		
+	
+			//Calcula a média usando a soma e divide por 4
 			double media = soma / 4;
 			System.out.println("Media de" + nome + ":" + media);
 			
+			// Verifica a situação do aluno para indicar o caminho que vai seguir baseado no 
+			//resultado da media obtida.
 			if (media >= 7) {
 				System.out.println("Aprovado Guerreiro");
 			} else if (media >= 5) {
@@ -40,6 +49,7 @@ Scanner teste = new Scanner(System.in);
 				System.out.println("Reprovado, estude mais meu jovem gafanhoto");			
 			}
 			
+			//Parte para verificação do pior e melhor aluno, ir fazendo a subscrição e guardar para imprimir no final
 			if (media > mediaMelhor) {
 				mediaMelhor = media;
 				nomeMelhor = nome;
@@ -50,9 +60,11 @@ Scanner teste = new Scanner(System.in);
 				nomePior = nome;
 			}
 			
+			//apenas para criar um a separação
 			System.out.println(".....");
 		}
 		
+		//indica no final de informar os 10 alunos quem foi o aluno com melhor nota e pior nota.
 		System.out.println("Aluno com melhor nota: ");
 		System.out.println("Nome: " + nomeMelhor + " Media:" + mediaMelhor);
 		
@@ -61,9 +73,7 @@ Scanner teste = new Scanner(System.in);
 		
 		teste.close();
 			}
-	}
+}
 
-
-	}
-
+}
 }
